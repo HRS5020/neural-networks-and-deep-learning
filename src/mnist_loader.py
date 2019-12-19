f097412c946314f2582d6e6de868b27adf98aa15
HRS5020 <hrsolanki1111@gmail.com>
2019-12-19 23:04:35 +0100

    python 3.7 compatible

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -10,11 +10,12 @@ function usually called by our neural network code.
 
 #### Libraries
 # Standard library
-import cPickle
+import pickle as cPickle
 import gzip
 
 # Third-party libraries
 import numpy as np
+
 
 def load_data():
     """Return the MNIST data as a tuple containing the training data,
@@ -39,9 +40,11 @@ def load_data():
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
     """
-    f = gzip.open('../data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
-    f.close()
+    # f = gzip.open('../data/mnist.pkl.gz', 'rb')
+    # training_data, validation_data, test_data = cPickle.load(f, encoding='bytes')
+    # f.close()
+    with gzip.open('../data/mnist.pkl.gz', 'rb') as f:
+        training_data, validation_data, test_data = cPickle.load(f, encoding='latin1')
     return (training_data, validation_data, test_data)
 
 def load_data_wrapper():
@@ -68,12 +71,12 @@ def load_data_wrapper():
     tr_d, va_d, te_d = load_data()
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
-    training_data = zip(training_inputs, training_results)
+    training_data = list(zip(training_inputs, training_results))
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = zip(validation_inputs, va_d[1])
+    validation_data = list(zip(validation_inputs, va_d[1]))
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = zip(test_inputs, te_d[1])
-    return (training_data, validation_data, test_data)
+    test_data = list(zip(test_inputs, te_d[1]))
+    return training_data, validation_data, test_data
 
 def vectorized_result(j):
     """Return a 10-dimensional unit vector with a 1.0 in the jth
